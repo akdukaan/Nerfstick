1d486056d2d90046742f12cfad3d1a633d8cba35
Kaan <drbot7@gmail.com>
2024-08-03 00:54:38 -0500

    Update for 1.21

--- src/main/java/com/birdflop/nerfstick/NerfstickListener.java
+++ src/main/java/com/birdflop/nerfstick/NerfstickListener.java
@@ -4,16 +4,17 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.component.DebugStickState;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.Property;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_20_R3.CraftWorld;
-import org.bukkit.craftbukkit.v1_20_R3.block.CraftBlock;
-import org.bukkit.craftbukkit.v1_20_R3.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.block.CraftBlock;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -26,25 +27,30 @@ import java.util.Collection;
 import java.util.List;
 
 public class NerfstickListener implements Listener {
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onPlayerInteract(PlayerInteractEvent event) {
         // Check if the player is holding a debug stick
         ItemStack itemStack = event.getItem();
-        if (itemStack == null || itemStack.getType() != Material.DEBUG_STICK)
-            return;
-
-        event.setCancelled(true);
+        if (itemStack == null || itemStack.getType() != Material.DEBUG_STICK) return;
 
         // Get the block the player is looking at
         Block block = event.getClickedBlock();
-        if (block == null)
-            return;
+        if (block == null) return;
+
+        // Get the action
+        Action action = event.getAction();
+        if (action != Action.LEFT_CLICK_BLOCK && action != Action.RIGHT_CLICK_BLOCK) return;
+
+        event.setCancelled(true);
+
+        // Get the player
+        Player player = event.getPlayer();
 
         // Check if the modification is allowed
-        String denyReason = Permission.getBlockProtection(event.getPlayer(), block.getLocation(), block.getType());
+        String denyReason = Permission.getBlockProtection(player, block.getLocation(), block.getType());
         if (denyReason != null && !denyReason.isEmpty()) {
             // Tell player about the error
-            event.getPlayer().sendActionBar(
+            player.sendActionBar(
                     Component.empty()
                             .append(Component.text("Interaction denied! Reason: ", TextColor.color(0xFFAA00)))
                             .append(Component.text(denyReason, TextColor.color(0xFF5555)))
@@ -52,26 +58,19 @@ public class NerfstickListener implements Listener {
             return;
         }
 
-        // Get block id
-        String blockId = block.getType().getKey().toString();
-
         // Get NMS data
-        net.minecraft.world.item.ItemStack nmsItemStack = CraftItemStack.asNMSCopy(itemStack);
+        CraftItemStack craftItemStack = (CraftItemStack) itemStack;
         BlockState blockState = ((CraftBlock) block).getNMS();
         net.minecraft.world.level.block.Block nmsBlock = blockState.getBlock();
-
-        // Get state definition
         StateDefinition<net.minecraft.world.level.block.Block, BlockState> stateDefinition = nmsBlock.getStateDefinition();
-        Collection<Property<?>> properties = stateDefinition.getProperties();
 
         // Filter properties
+        Collection<Property<?>> properties = stateDefinition.getProperties();
+        String blockId = block.getType().getKey().toString();
         List<Property<?>> propertyList = properties.stream().filter(property -> {
             String propertyName = property.getName();
-            return Permission.allowBlockState(event.getPlayer(), blockId, propertyName);
+            return Permission.allowBlockState(player, blockId, propertyName);
         }).toList();
-
-        // Get the player
-        Player player = event.getPlayer();
 
         // Check if the block has any block state
         if (propertyList.isEmpty()) {
@@ -86,31 +85,24 @@ public class NerfstickListener implements Listener {
         }
 
         // Get the property
-        CompoundTag nbtTagCompound = nmsItemStack.getOrCreateTagElement("DebugProperty");
-        String propertyName = nbtTagCompound.getString(blockId);
-        Property<?> property = stateDefinition.getProperty(propertyName);
-
-        // Check if the property is valid
-        if (property == null || propertyList.stream().noneMatch(p -> p.getName().equals(propertyName)))
-            property = propertyList.get(0); // Get first property
-
-        // Get the action
-        Action action = event.getAction();
-
-        if (action != Action.LEFT_CLICK_BLOCK && action != Action.RIGHT_CLICK_BLOCK)
-            return;
+        DebugStickState debugStickState = craftItemStack.handle.get(DataComponents.DEBUG_STICK_STATE);
+        if (debugStickState == null) return;
+        Property<?> property = debugStickState.properties().getOrDefault(blockState.getBlockHolder(), propertyList.getFirst());
+        String propertyName = property.getName();
+        if (propertyList.stream().noneMatch(p -> p.getName().equals(propertyName))) {
+            property = propertyList.getFirst();
+        }
 
         boolean inverse = player.isSneaking();
-        boolean setState = action == Action.LEFT_CLICK_BLOCK;
-
-        if (setState) {
+        if (action == Action.LEFT_CLICK_BLOCK) {
             property = getRelative(propertyList, property, inverse);
 
             // Update property name
-            nbtTagCompound.putString(blockId, property.getName());
+            debugStickState = debugStickState.withProperty(blockState.getBlockHolder(), property);
+            craftItemStack.handle.set(DataComponents.DEBUG_STICK_STATE, debugStickState);
 
             // Get the bukkit copy
-            ItemStack bukkitItemStack = CraftItemStack.asBukkitCopy(nmsItemStack);
+            ItemStack bukkitItemStack = craftItemStack.handle.asBukkitCopy();
 
             // Update the item in the player's hand
             player.getInventory().setItemInMainHand(bukkitItemStack);
@@ -143,7 +135,7 @@ public class NerfstickListener implements Listener {
         }
     }
 
-    // Weird workaround for Java generics (I hate Java)
+    // Weird workaround for Java generics
     private static <T extends Comparable<T>> BlockState cycleState(BlockState state, Property<T> property, boolean inverse) {
         return state.setValue(property, getRelative(property.getPossibleValues(), state.getValue(property), inverse));
     }
